89e66cbd86a1c42bd4255eab55cb38fe666d88b0
zyq <9130@mail>
2023-07-24 21:28:50 +0800

    车辆统计信息列表数据获取方式修改

--- src/views/carTrajectory.vue
+++ src/views/carTrajectory.vue
@@ -17,6 +17,20 @@
                 <el-input placeholder="等待加载" v-model="pointTotalNumber" :disabled="true" style="width: 300px;margin-left: 20px">
                     <template slot="prepend">轨迹点总数:</template>
                 </el-input>
+              <el-date-picker
+                  v-model="timeRange"
+                  type="datetimerange"
+                  range-separator="至"
+                  start-placeholder="开始日期"
+                  end-placeholder="结束日期"
+                  format="yyyy-MM-dd HH:mm:ss"
+                  value-format="yyyy-MM-dd HH:mm:ss"
+                  @change="changRange"
+              />
+              <el-button
+                type="primary"
+                @click="getCarInfoList"
+              >查询</el-button>
             </div>
             <div >
                 <el-tag type="warning" class="table_header" effect="dark">车辆列表</el-tag>
@@ -24,9 +38,10 @@
                         v-loading="car_loading"
                         highlight-current-row
                         element-loading-text="加载数据中"
-                        :data="carTable.slice((car_current_page-1) * 10, car_current_page * 10)"
+                        :data="carTable"
                         ref="carTable"
                         @row-click="handleRowClick"
+                        @sort-change="sortChange"
                         style="width: 100%">
                     <el-table-column
                             prop="carNumber"
@@ -138,11 +153,13 @@
                     </el-table-column>
                 </el-table>
                 <el-pagination
-                        :current-page="car_current_page"
-                        :total='carTable.length'
+                        :current-page="pageNum"
+                        :total='total'
                         layout="prev, pager, next"
                         @current-change="handleCarPageChange"
-                        :page-size="12"
+                        @next-click = "handleNextClick"
+                        @prev-click = "handlePrevClick"
+                        :page-size="pageSize"
                 >
                 </el-pagination>
             </div>
@@ -166,7 +183,20 @@ export default {
             showChart: false,
             carTable: [],
             carInfo: null,
-            car_current_page: 1,
+            //当前页码
+            pageNum: 1,
+            //当前页码
+            pageSize: 10,
+            //数据总数
+            total: 0,
+            //排序字段
+            orderBy: null,
+            //是否降序
+            isDesc: false,
+            //过滤时间范围
+            timeRange: ['2021-02-01 00:00:00','2021-02-01 23:59:59'],
+            //车辆的统计信息
+            carStat: null
         }
     },
     methods: {
@@ -202,42 +232,79 @@ export default {
             let eChart = this.$echarts.init(document.getElementById(domId));
             eChart.setOption(option)
         },
-        handleRowClick(row) {
-            // this.showForm = true
-            // this.tra.tableName = row.tableName
-            console.log(row)
-            let keyArray = ['0-30min', '30min-1h', '1h-2h', '2h<']
-            this.initBarChart("timeDistributeChart", '行程时间分布', keyArray, row.traTimeDistribute)
-            keyArray = ["0-1km","1-2km","3-4km","5-6km","7-8km","8-9km","9-10km","10-20km","20-30km","30-40km","40-50km","50-60km","60-70km","70-80km","80-90km","90-100km","100km<"]
+      handleRowClick(row) {
+          this.axios.post("/stat/getStatByCar",{
+            "carNumber": row.carNumber,
+            "minTime": this.timeRange[0],
+            "maxTime": this.timeRange[1],
+          }).then(res => {
+            let stat = res.data.msg
+
+            let traTimeDistribute = stat['traTimeDistribute']
+            let keyArray = Object.keys(traTimeDistribute)
+
+            this.initBarChart("timeDistributeChart", '行程时间分布', keyArray, traTimeDistribute)
+
+            let traDistanceDistribute = stat['traTimeDistribute'];
+            keyArray = Object.keys(traDistanceDistribute)
+
+            let flag = false
             let tempDistanceDistribute = {}
             let tempKeyArray = []
-            let flag = false
             keyArray.forEach((item) => {
-                if(flag){
-                    tempDistanceDistribute["30km<"] += row.traDistanceDistribute[item]
+              if(flag){
+                tempDistanceDistribute["30km<"] += row.traDistanceDistribute[item]
+              }
+              else {
+                if(item == "30-40km"){
+                  tempDistanceDistribute["30km<"] = row.traDistanceDistribute[item]
+                  tempKeyArray.push("30km<")
+                  flag = true
                 }
                 else {
-                    if(item == "30-40km"){
-                        tempDistanceDistribute["30km<"] = row.traDistanceDistribute[item]
-                        tempKeyArray.push("30km<")
-                        flag = true
-                    }
-                    else {
-                        tempDistanceDistribute[item] = row.traDistanceDistribute[item]
-                        tempKeyArray.push(item)
-                    }
+                  tempDistanceDistribute[item] = row.traDistanceDistribute[item]
+                  tempKeyArray.push(item)
                 }
+              }
             })
             this.initBarChart("distanceDistributeChart", '行程距离分布', tempKeyArray, tempDistanceDistribute)
-            keyArray = ["00","01","02","03","04","05","06","07","08","09","10","11","12","13","14","15","16","17","18","19","20","21","22","23"]
-            this.initBarChart("hourDistributeChart", '行程出发时间分布/h', keyArray, row.traStartTimePerHour)
-            keyArray = ["01","02","03","04","05","06","07","08","09","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24","25","26","27","28","29","30","31"]
-            this.initBarChart("dayDistributeChart", '行程出发时间分布/day', keyArray, row.traStartTimePerDay)
+
+            let traStartTimePerHour = stat['traStartTimePerHour'];
+            keyArray = Object.keys(traStartTimePerHour)
+            this.initBarChart("hourDistributeChart", '出发时间分布/h', keyArray, traStartTimePerHour)
+
+            let traStartTimePerDay = stat['traStartTimePerDay'];
+            keyArray = Object.keys(traStartTimePerDay)
+            this.initBarChart("dayDistributeChart", '出发时间分布/day', keyArray, traStartTimePerDay)
 
             this.showChart = true
+          }).catch(err => {
+            console.log(err)
+          })
         },
         handleCarPageChange(val) {
-            this.car_current_page = val
+          this.pageNum = val
+          this.getCarInfoList()
+        },
+        changRange(timeRange){
+          this.timeRange = timeRange
+        },
+        handlePrevClick(val){
+          if(val > 1){
+            this.pageNum = val - 1
+            this.getCarInfoList()
+          }
+        },
+        handleNextClick(val){
+          if(val * this.pageSize > this.total){
+            this.pageNum = val + 1
+            this.getCarInfoList()
+          }
+        },
+        sortChange(column){
+          this.orderBy = column.prop
+          this.isDesc = column.order === "descending"
+          this.getCarInfoList()
         },
         goDetail(index, rows) {
             let row = rows[index]
@@ -245,17 +312,17 @@ export default {
             this.$router.push({path:'/carDetail',query:{carNumber:row.carNumber}});
         },
         getCarInfoList() {
-            this.carTable = []
             let loading = Loading.service({ fullscreen: true, text: 'Loading'});
-            this.axios.get("/camTra/getCarInfoList").then(res => {
-                this.carTotalNumber = res.data.msg.carCount
-                this.pointTotalNumber = res.data.msg.pointCount
-                this.traTotalNumber = res.data.msg.traCount
-                for(const key in res.data.msg.carMap)
-                {
-                    let tempCar = res.data.msg.carMap[key]
-                    this.carTable.push(tempCar)
-                }
+            this.axios.post("/stat/getStatList",{
+              "pageNum": this.pageNum,
+              "pageSize":this.pageSize,
+              "minTime": this.timeRange[0],
+              "maxTime": this.timeRange[1],
+              "orderBy": this.orderBy,
+              "isDesc": this.isDesc
+            }).then(res => {
+              this.carTable = res.data.msg.records
+              this.total = res.data.msg.total
                 setTimeout(() => {
                     loading.close();
                 }, 1000);
@@ -267,9 +334,20 @@ export default {
         fun(val){
             return Number(val).toFixed(2);
         },
+        getTotal(){
+          //todo 获取车辆总数、轨迹总数的接口未完成，待更新
+          this.axios.get("/stat/info").then(res => {
+            this.carTotalNumber = res.data.msg.records
+            this.total = res.data.msg.total
+          }).catch(err => {
+            console.log(err)
+          })
+        }
     },
     created() {
+        this.pageNum = 1
         this.getCarInfoList()
+        // this.getTotal();
     },
     mounted() {
     },
